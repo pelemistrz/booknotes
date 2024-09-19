4d29955be7af61828a19829d0a4a5c74a65b3b3b
Marcin Pająk <39952424+pelemistrz@users.noreply.github.com>
2024-09-19 11:05:12 +0200

    Set up project

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import pg from "pg";
+import bodyParser from "body-parser";
+import env from "dotenv";
+
+const app = express();
+const port = 3000;
+env.config();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+db.connect();
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Running server on port ${port}`);
+});
